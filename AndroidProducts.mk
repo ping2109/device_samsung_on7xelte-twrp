fd3fbf4a75c5eb58c4c6899a8d16787e324f7ade
pingmado <75196272+ping2109@users.noreply.github.com>
2022-03-12 20:15:18 +0700

    drop deprecated add lunch combo

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,10 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/samsung/on7xelte
+PRODUCT_MAKEFILES := \
+     $(LOCAL_DIR)/omni_on7xelte.mk
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
-PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_on7xelte.mk
+COMMON_LUNCH_CHOICES := \
+    omni_on7xelte-user \
+    omni_on7xelte-userdebug \
+    omni_on7xelte-eng
